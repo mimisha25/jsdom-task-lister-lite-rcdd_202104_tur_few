439eeb7ba729cd4369c3e03bc442af928dbd8293
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-17 11:30:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-
-let input = document.getElementById("new-task-description");
- let button = document.getElementById("btnId");
- let toDos = document.getElementById("tasks");
-
-   button.addEventListener('click', (e) => {
-     let li = document.createElement("li");
-     toDos.appendChild(li);
-     li.innerText = input.value;
+  const inputtasks = document.getElementById('new-task-description');
+   const form = document.getElementById('create-task-form');
+   const list = document.getElementById('list');
+   const task = document.getElementById('tasks')
+   form.addEventListener('submit', (e) => {
      e.preventDefault();
-   })
-
+     let inputText = inputtasks.value;
+     let li = document.createElement('li');
+     li.innerText = inputText;
+     task.append(li);
+   });
  });
-});
