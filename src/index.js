d632163655d47b2f33f998fe1178ce7a18a52969
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-17 11:54:42 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById("create-task-form");
+   const list = document.getElementById("tasks");
+   const item = document.getElementById("new-task-description");
+   const removeTaks = document.getElementById("removeTask");
+
+   form.addEventListener("submit", (e)=>{
+     e.preventDefault();
+     const newItem = document.createElement("li");
+     newItem.innerHTML = item.value;
+     list.append(newItem);
+   })
+   removeTaks.addEventListener("submit", function(e){
+       const li = document.querySelectorAll();
+       for (let i = 0; i < li.length; i++){
+         if (item.value === li[i].innerHTML){
+           list.removeChild(li[i]);
+         }
+       }
+       e.preventDefault();
+     });
+
+   });
