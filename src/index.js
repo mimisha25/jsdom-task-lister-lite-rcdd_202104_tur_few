b4f2487ba5970037cfe9ba8abc94b579c6face3a
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-17 11:53:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const form = document.querySelector('form');
+  const form = document.getElementById("create-task-form");
+   const list = document.getElementById("tasks");
+   const item = document.getElementById("new-task-description");
+   const removeTaks = document.getElementById("removeTask");
 
-   form.addEventListener('submit', function(event){
-     event.preventDefault();
-
-     let inputTag = document.querySelector('input#new-task-description');
-     let inputValue = inputTag.value;
-
-
-     let newLi = document.createElement('li');
-     newLi.innerText = inputValue;
-
-     let ulTag = document.querySelector('ul#tasks');
-     ulTag.append(newLi);
-     inputTag.value = '';
-
+   form.addEventListener("submit", (e)=>{
+     e.preventDefault();
+     const newItem = document.createElement("li");
+     newItem.innerHTML = item.value;
+     list.append(newItem);
    })
- });
+   removeTaks.addEventListener("submit", function(e){
+       const li = document.querySelectorAll();
+       for (let i = 0; i < li.length; i++){
+         if (item.value === li[i].innerHTML){
+           list.removeChild(li[i]);
+         }
+       }
+       e.preventDefault();
+     });
+
+   });
