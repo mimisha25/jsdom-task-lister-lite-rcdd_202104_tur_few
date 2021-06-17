cbdb4885ec3bb5a557da95c11113453741a98a77
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-17 11:21:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+
+  let input = document.getElementById("new-task-description");
+ let button = document.getElementById("btnId");
+ let toDos = document.getElementById("tasks");
+
+   button.addEventListener('click', (e) => {
+     let li = document.createElement("li");
+     toDos.appendChild(li);
+     li.innerText = input.value;
+     e.preventDefault();
+   })
+
+ });
 });
