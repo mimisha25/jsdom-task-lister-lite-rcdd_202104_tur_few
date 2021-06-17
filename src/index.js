5c2b21129b656128c15be56d10da4c135e2b85a4
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-17 11:39:59 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const form = document.getElementById("create-task-form");
-   const item = document.getElementById("new-task-description");
-   const list = document.getElementById("tasks");
+  const submitButton = document.getElementById("submitForm");
+   const tasks = document.getElementById("tasks");
+   const newTask = document.getElementById("new-task-description");
    const removeTaks = document.getElementById("removeTask");
 
-   form.addEventListener("submit", (e)=>{
+   submitButton.addEventListener("click", function(e){
+     let taskLi = document.createElement('li');
+     taskLi.textContent = newTask.value;
+     tasks.append(taskLi);
      e.preventDefault();
-     const newItem = document.createElement("li");
-     newItem.innerHTML = item.value;
-     list.append(newItem);
-   })
-   removeTaks.addEventListener("submit", function(e){
-       const li = document.querySelectorAll();
-       for (let i = 0; i < li.length; i++){
-         if (item.value === li[i].innerHTML){
-           list.removeChild(li[i]);
-         }
-       }
-       e.preventDefault();
-     });
-
    });
+
+   removeTaks.addEventListener("click", function(e){
+     const li = document.querySelectorAll();
+     for (let i = 0; i < li.length; i++){
+       if (newTask.value === li[i].textContent){
+         tasks.removeChild(li[i]);
+       }
+     }
+     e.preventDefault();
+   });
+
+
+
+ });
